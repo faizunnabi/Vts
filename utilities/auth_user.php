41db59e05578aca4f301a171d6ecaaaad68413c7
Syed Faizun Nabi <faizunnabi@users.noreply.github.com>
2017-09-11 14:58:43 +0400

    Update auth_user.php

--- utilities/auth_user.php
+++ utilities/auth_user.php
@@ -4,7 +4,7 @@ $username = $_POST['uname'];
 $pass = $_POST['upass'];
 
 if($_POST && $_SERVER['REQUEST_METHOD'] == 'POST'){
-	$db = new PDO('mysql:host=localhost;dbname=Tracker;charset=utf8mb4', 'root', 'rumaithy');
+	$db = new PDO('mysql:host='';dbname='';charset=utf8mb4', '', '');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	$stmt = $db->prepare("SELECT * FROM users where username = :uname and password = :upass");
