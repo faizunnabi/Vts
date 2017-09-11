08776fd307671769087f0cae2452471db3f12073
Syed Faizun Nabi <faizunnabi@users.noreply.github.com>
2017-09-11 15:00:24 +0400

    Update machine.php

--- utilities/machine.php
+++ utilities/machine.php
@@ -6,17 +6,17 @@
  * Time: 9:52 AM
  */
 
-$host = "localhost";
-$user = "root";
-$pass = "rumaithy";
-$db = "Tracker"; 
+$host = "";
+$user = "";
+$pass = "";
+$db = ""; 
 /*if($_GET['status']==""){
 	$qry =="";
 }else{
 	$qry = "where online_status="+$_GET['status'];
 }*/
 $data = array();
-$db = new PDO('mysql:host=localhost;dbname=Tracker;charset=utf8mb4', 'root', 'rumaithy');
+$db = new PDO('mysql:host='';dbname='';charset=utf8mb4', '', '');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $stmt = $db->query("SELECT * FROM machines order BY name");
