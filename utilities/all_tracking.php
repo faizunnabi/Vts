0d4bd16cabc87a072fd2bdd6283431281396b87a
Syed Faizun Nabi <faizunnabi@users.noreply.github.com>
2017-09-11 14:58:06 +0400

    Update all_tracking.php

--- utilities/all_tracking.php
+++ utilities/all_tracking.php
@@ -1,7 +1,7 @@
 <?php
 
 $data = array();
-$db = new PDO('mysql:host=localhost;dbname=Tracker;charset=utf8mb4', 'root', 'rumaithy');
+$db = new PDO('mysql:host='';dbname='';charset=utf8mb4', '', '');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $stmt = $db->query("SELECT d.status,m1.* FROM devices d,new_tracker m1 LEFT JOIN new_tracker m2 ON (m1.imei = m2.imei AND m1.id < m2.id) WHERE m2.id IS NULL and d.imei=m1.imei");
